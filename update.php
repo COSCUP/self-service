50b88e98adad27c3046f39271c9aed3406af477d
Evelyn Hung <jj.evelyn@gmail.com>
2014-05-19 01:26:03 +0800

    set correct content type

--- update.php
+++ update.php
@@ -1,6 +1,13 @@
 <?php
 include_once('config.php');
 
+header('Content-Type: text/plain');
+setlocale(LC_ALL, 'en_US.UTF-8');
+
+if (!isset($argv) && $_SERVER['REQUEST_METHOD'] !== 'POST') {
+    die("Error: Not a POST request nor run from command line.");
+}
+
 system('cd ' . DEPLOY_PATH. '; php update.php');
 
 ?>
